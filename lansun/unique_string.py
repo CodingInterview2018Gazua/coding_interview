fc6f4989ef50d55182bea2661650f20619907b72
amugae <amugae.00001@gmail.com>
2018-08-15 09:44:36 +0900

    문제풀이

---
+++ lansun/unique_string.py
@@ -0,0 +1,34 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 1.1 문자열에 포함된 문자들이 전부 유일한지 검사하는 알고리즘을 구현하여라 다른 자료구조를 사용할 수 없다.
+# ASCII Code
+
+
+def is_unique_string(string):
+    string = string.strip()
+    if len(string) > 256:
+        return False
+
+    checker = 0
+    for char in string:
+        char_to_digit = ord(char.lower()) - ord('a')
+        shift = 1 << char_to_digit
+        if (checker & shift) > 0:
+            return False
+        checker |= shift
+
+    return True
+
+
+print is_unique_string("abcdef")
+print is_unique_string("abcdeffff")
+print is_unique_string("abcdeffff ")
+print is_unique_string(" abcdef ")
+
+
+
+
+
+
+
