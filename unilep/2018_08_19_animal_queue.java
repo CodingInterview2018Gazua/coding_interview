0d9e9c36cc80bfd12dcb36ade7e5ef5c75f4a088
sirius <unilep@naver.com>
2018-08-19 21:27:15 +0900

    Create 2018_08_19_animal_queue.java

---
+++ unilep/2018_08_19_animal_queue.java
@@ -0,0 +1,102 @@
+package algorithm;
+
+import java.io.PrintStream;
+import java.util.LinkedList;
+
+public class Main {
+
+	static PrintStream ps = System.out;
+
+	public static void main(String[] args) {
+		
+		Queue<Animal> queue = new Queue<>();
+		queue.enqueue(new Dog("A"));
+		queue.enqueue(new Cat("B"));
+		queue.enqueue(new Dog("C"));
+		queue.enqueue(new Cat("D"));
+		queue.enqueue(new Cat("E"));
+		
+		ps.println(queue.dequeueDog().getName()); // C
+		
+		ps.println(queue.dequeueAny().getName()); // E
+		
+		ps.println(queue.dequeueDog().getName()); // A
+		
+		ps.println(queue.dequeueCat().getName()); // D
+		
+		ps.println(queue.dequeueAny().getName()); // B
+	}
+}
+
+class Animal { 
+	private boolean out;
+	private String name;
+	
+	public Animal(String name) {
+		this.name = name;
+	}
+	
+	public boolean isOut() {
+		return out;
+	}
+
+	public void setOut(boolean out) {
+		this.out = out;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
+
+class Dog extends Animal {
+	public Dog(String name) {
+		super(name);
+	}
+}
+
+class Cat extends Animal { 
+	public Cat(String name) {
+		super(name);
+	}
+}
+
+class Queue<T extends Animal> {
+	
+	LinkedList<T> anyLinkedList = new LinkedList<>();
+	LinkedList<T> dogLinkedList = new LinkedList<>();
+	LinkedList<T> catLinkedList = new LinkedList<>();
+	
+	public void enqueue(T animal) {
+		anyLinkedList.add(animal);
+		if(animal instanceof Dog) {
+			dogLinkedList.add(animal);
+		} else {
+			catLinkedList.add(animal);
+		}
+	}
+	
+	public T dequeueAny() {
+		T animal = null;
+		while((animal = anyLinkedList.removeLast()).isOut());
+		animal.setOut(true);
+		return animal;
+		
+	}
+	
+	public T dequeueDog() {
+		T animal = null;
+		while((animal = dogLinkedList.removeLast()).isOut());
+		animal.setOut(true);
+		return animal;
+	}
+	
+	public T dequeueCat() {
+		T animal = null;
+		while((animal = catLinkedList.removeLast()).isOut());
+		animal.setOut(true);
+		return animal;
+	}
+	
+}
