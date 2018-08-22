979416b17e063c581f85c992f7a24db322aa5ea9
amugaes <amugae.00001@gmail.com>
2018-08-22 12:30:43 +0900

    4.2 문제풀이

--- lansun/chapter4/4.2_directed_graph.py
+++ lansun/chapter4/4.2_directed_graph.py
@@ -3,26 +3,60 @@
 
 # 4.2 주어진 유향 그래프(direct graph) 에서 특정한 두 노드간에 경로(route)가 존재하는지를 판별하는 알고리즘을 구현하라
 # bfs : breadth-first-search 너비우선탐색
+# 두 노드 가운데 한 노드를 선택하여 탐색 도중에 다른 노드가 발견되는지 검사하면 된다
+from collections import defaultdict
 
 
-def exist_bfs_paths(graph, start, goal):
-    queue = [(start, [start])]
-    result = []
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
 
-    while queue:
-        n, path = queue.pop(0)
-        if n == goal:
-            result.append(path)
-        else:
-            for m in graph[n] - set(path):
-                queue.append((m, path + [m]))
-    return True if len(result) > 0 else False
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
 
-graph = {'A': set(['B', 'C']),
-         'B': set(['A', 'D', 'E']),
-         'C': set(['A', 'F']),
-         'D': set(['B']),
-         'E': set(['B', 'F']),
-         'F': set(['C', 'E'])}
+    def BFS(self, start):
+        visited = [False] * (len(self.graph))
+        queue = []
 
-print exist_bfs_paths(graph, 'A', 'F') # True
+        queue.append(start)
+        visited[start] = True
+
+        while queue:
+            s = queue.pop(0)
+            for i in self.graph[s]:
+                if not visited[i]:
+                    queue.append(i)
+                    visited[i] = True
+                else:
+                    return True
+
+        return False
+
+
+graph = Graph()
+
+## ===> First example
+graph.add_edge(0, 1)
+graph.add_edge(0, 2)
+graph.add_edge(1, 2)
+graph.add_edge(2, 2)
+graph.add_edge(2, 3)
+graph.add_edge(3, 3)
+
+print graph.graph
+# defaultdict(<type 'list'>, {0: [1, 2], 1: [2], 2: [2, 3], 3: [3]})
+
+print graph.BFS(1)
+# True
+
+## ===> Second example
+graph.add_edge(0, 1)
+graph.add_edge(1, 2)
+graph.add_edge(2, 3)
+graph.add_edge(3, 4)
+graph.add_edge(4, 4)
+
+print graph.graph
+# defaultdict(<type 'list'>, {0: [1, 2, 1], 1: [2, 2], 2: [2, 3, 3], 3: [3, 4], 4: [4]})
+print graph.BFS(2)
+# True
