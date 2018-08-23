2a44f6a7b4cc4a3b89de73c713965965291d2c44
unilep <unilep@naver.com>
2018-08-23 21:49:52 +0900

    4.4

--- unilep/4_tree_graph/4.4 binary_tree_to_linkedlist.py
+++ unilep/4_tree_graph/4.4 binary_tree_to_linkedlist.py
@@ -8,36 +8,36 @@ class Node:
 		self.left = None
 		self.right = None
 		
-	def addLeft(self, data):
+	def add_left(self, data):
 		self.left = Node(data)
 	
-	def addRight(self, data):
+	def add_right(self, data):
 		self.right = Node(data)
 
 class LinkedList:
 	
-	def __init__(self, data):
+	def __init__(self, data = None):
 		self.data = data
 		self.next = None
+
+	def add_next(self, data):
+		temp_next = self
 	
-	def addNext(self, data):
-		tempNext = self
-		while tempNext.next is not None:
-			tempNext = tempNext.next
-			
-		tempNext.next = LinkedList(data)
-		
-	def setNext(self, node):
-		self.next = node
+		while temp_next.next is not None:
+			temp_next = temp_next.next
+	
+		temp_next.next = LinkedList(data)
 	
 	def __repr__(self):
-		tempNext = self
+		temp_next = self
 		ret = [ ]
-		while tempNext.next is not None:
-			ret.append(tempNext.data)
-			tempNext = tempNext.next
-		ret.append(tempNext.data)
-		return repr(ret)
+		while temp_next.next is not None:
+			ret.append(temp_next.data)
+			temp_next = temp_next.next
+
+		ret.append(temp_next.data)
+		
+		return "{}".format(ret)
 		
 def bfs(root):
 	q = Queue.Queue()
@@ -47,32 +47,41 @@ def bfs(root):
 	
 	while not q.empty():
 		size = q.qsize()
-		linkedlist = LinkedList('')
 		
+		linked_list = None
 		array = [ ]
+		
 		for i in range(size):
 			node = q.get()
-			linkedlist.addNext(node.data)
-			if node.left is not None:
-				q.put(node.left)
-			if node.right is not None:
-				q.put(node.right)
+			if i is 0: linked_list = LinkedList(node.data)
+			else: linked_list.add_next(node.data)
+			
+			if node.left is not None: q.put(node.left)
+			if node.right is not None: q.put(node.right)
 				
-		root_list.append(linkedlist)
+		root_list.append(linked_list)
 		
 	return root_list
 		
-		
 root = Node(1)
-root.addLeft(3)
-root.addRight(5)
+root.add_left(3)
+root.add_right(5)
 
-root.left.addLeft(10)
-root.left.addRight(4)
+root.left.add_left(10)
+root.left.add_right(4)
 
-root.right.addLeft(2)
-root.right.addRight(6)
+root.right.add_left(2)
+root.right.add_right(6)
+
+root.left.left.add_left(1)
+root.left.left.add_right(2)
+root.left.right.add_left(3)
+root.left.right.add_left(4)
+
+root.right.left.add_left(5)
+root.right.left.add_right(6)
+root.right.right.add_left(7)
+root.right.right.add_right(8)
 
 result = bfs(root)
-print repr(result)
-
+print result
