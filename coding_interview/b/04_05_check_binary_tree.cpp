419dede60af952c921dbf3c56154a7262074a945
bruce <bruce.kim.it@gmail.com>
2018-08-27 21:48:55 +0900

    04_05 fix

--- coding_interview/b/04_05_check_binary_tree.cpp
+++ coding_interview/b/04_05_check_binary_tree.cpp
@@ -3,53 +3,44 @@
 
 using namespace std;
 
+#define MIN 0x8F3F'3F3F
+#define MAX 0x3F3F'3F3F
+
 struct Node {
-    Node(int _k)
-        : k(_k), left(nullptr), right(nullptr) {
-    }
+    Node(int _k) : k(_k), left(nullptr), right(nullptr) {}
     int k;
     Node *left, *right;
 };
 
-bool checkBalance(Node *root) {
-    stack<Node *> s;
-    s.push(root);
-    while (!s.empty()) {
-        Node *t = s.top();
-        if (t->left != nullptr && t->right != nullptr) {
-            if (t->left->k >= t->k || t->right->k <= t->k) {
-                return false;
-            }
-        }
-        s.pop();
-        if (t->right != nullptr) {
-            s.push(t->right);
-        }
-        if (t->left != nullptr) {
-            s.push(t->left);
-        }
+bool isBalancedUtil(Node *node, int min, int max) {
+    if (node == nullptr) {
+        return true;
     }
-    return true;
+    if (node->k < min || node->k > max) {
+        cout << "fail: " << node->k << " " << min << " " << max << endl;
+        return false;
+    }
+    return isBalancedUtil(node->left, min, node->k - 1) &&
+           isBalancedUtil(node->right, node->k + 1, max);
+}
+
+bool isBalanced(Node *root) {
+    return isBalancedUtil(root, MIN, MAX);
 }
 void test() {
+    /*         5
+          3         9
+       2    6     4     11
+    */
     Node *root = new Node(5);
-    Node *one = new Node(1);
-    Node *two = new Node(2);
-    Node *three = new Node(3);
-    Node *seven = new Node(7);
-    Node *seven2 = new Node(7);
+    root->left = new Node(3);
+    root->left->left = new Node(2);
+    root->left->right = new Node(6);
+    root->right = new Node(9);
+    root->right->left = new Node(4);
+    root->right->right = new Node(11);
 
-    root->left = three;
-    root->right = seven;
-    three->left = one;
-    cout << checkBalance(root) << "\n";
-
-    three->right = two;
-    cout << checkBalance(root) << "\n";
-
-    three->right = nullptr;
-    seven->right = seven2;
-    cout << checkBalance(root) << "\n";
+    cout << isBalanced(root) << "\n";
 }
 int main() {
     test();
