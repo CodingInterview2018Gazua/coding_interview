dcace82cd0a39c9987a2b48056944871accaf97d
amugaes <amugae.00001@gmail.com>
2018-08-28 20:13:09 +0900

    백준 1716 raw_input() 공백처리를 위한 strip 제거

--- baekjoon/lansun/0.2_vigenere_cipher.py
+++ baekjoon/lansun/0.2_vigenere_cipher.py
@@ -14,11 +14,12 @@ def vigenere_cipher(words, encrypt):
 
     for i in range(len(list(words))):
         char = words[i]
-
+        # print "[{}]".format(char)
         if char is not ' ':
-            char_ord = ord(words[i]) - ord('a')
+            char_ord = ord(char) - ord('a')
             key = ord(encrypt[i % len(encrypt)]) - 96 # loveove
             result = char_ord - key + 26 if char_ord - key < ord('a') else char_ord - key
+            print encrypt[i % len(encrypt)]
             encrypt_words.append(alphabets[result])
         else:
             encrypt_words.append(' ')
@@ -26,6 +27,6 @@ def vigenere_cipher(words, encrypt):
     return ''.join(encrypt_words)
 
 
-words = raw_input().strip()
-encrypt = raw_input().strip()
+words = raw_input()
+encrypt = raw_input()
 print vigenere_cipher(words, encrypt)
