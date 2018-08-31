c440a9d85cfbe650c31cf22b13c0a29d95601ae9
amugaes <amugae.00001@gmail.com>
2018-08-31 18:03:20 +0900

    백준 창영이의 일기장 문제풀이

---
+++ baekjoon/lansun/0.5_changyeons's_diary_2954.py
@@ -0,0 +1,24 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+https://www.acmicpc.net/problem/2954
+"""
+
+
+def revert_diary(words):
+    vowels = 'aeiou' # 모음 
+    vowels_patterns = [''.join([vowel, 'p', vowel]) for vowel in vowels]
+
+    for i in range(len(vowels_patterns)):
+        words = words.replace(vowels_patterns[i], vowels_patterns[i][:1])
+
+    return words
+
+
+words = raw_input().split(' ')
+result = []
+for i in range(len(words)):
+    result.append(revert_diary(words[i]))
+
+print ' '.join(result)
