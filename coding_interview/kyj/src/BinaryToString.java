a3027c514a4344983486437fdbd71133d72cf28b
Kimyongjin <yongjin5184@gmail.com>
2018-09-04 08:16:00 +0900

    5-2. BinaryToString

---
+++ coding_interview/kyj/src/BinaryToString.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * Created by yong on 2018. 9. 4..
+ */
+public class BinaryToString {
+    public static String printBinary(double num) {
+        if (num >= 1 || num <= 0) {
+            return "ERROR";
+        }
+
+        StringBuilder binary = new StringBuilder().append(".");
+        while (num > 0) {
+            if (binary.length() > 32) {
+                return "ERROR";
+            }
+
+            double r = num * 2;
+
+            if (r >= 1) {
+                binary.append(1);
+                num = r - 1;
+            } else {
+                binary.append(0);
+                num = r;
+            }
+        }
+        return binary.toString();
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Double input = Double.parseDouble(st.nextToken());
+        String result = printBinary(input);
+        System.out.println(result);
+    }
+}
