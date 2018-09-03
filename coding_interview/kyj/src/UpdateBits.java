727314563133eae4b123b4238a50e2239caaa747
Kimyongjin <yongjin5184@gmail.com>
2018-09-03 22:48:29 +0900

    first commit

---
+++ coding_interview/kyj/src/UpdateBits.java
@@ -0,0 +1,12 @@
+/**
+ * Created by yong on 2018. 9. 3..
+ */
+public class UpdateBits {
+    public static void main(String[] args){
+        System.out.println("UpdateBits");
+    }
+
+    public static int updateBits(long n, int m, int i, int j) {
+        return 1;
+    }
+}
