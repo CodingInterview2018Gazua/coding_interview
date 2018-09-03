cf7a915310646a2556ac595eedffec6cc58d2bae
Kimyongjin <yongjin5184@gmail.com>
2018-09-04 08:24:01 +0900

    5-2. 주석 추가

--- coding_interview/kyj/src/BinaryToString.java
+++ coding_interview/kyj/src/BinaryToString.java
@@ -7,12 +7,19 @@ import java.util.StringTokenizer;
  * Created by yong on 2018. 9. 4..
  */
 public class BinaryToString {
+    /**
+     *
+     * 5-2
+     * Input  : 0과 1 사이 실수, Double 타입
+     * Output : 이진수 형태의 String 타입
+     *
+     */
     public static String printBinary(double num) {
         if (num >= 1 || num <= 0) {
             return "ERROR";
         }
 
-        StringBuilder binary = new StringBuilder().append(".");
+        StringBuilder binary = new StringBuilder().append("0.");
         while (num > 0) {
             if (binary.length() > 32) {
                 return "ERROR";
