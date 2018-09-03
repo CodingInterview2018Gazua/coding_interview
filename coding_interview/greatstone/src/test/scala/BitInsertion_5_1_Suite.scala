eada85a7bf0ae9aa9f385a07049ae4921a388363
김대석 <greatstone@satreci.com>
2018-09-03 23:05:14 +0900

    코딩 인터뷰 5.1 풀이

---
+++ coding_interview/greatstone/src/test/scala/BitInsertion_5_1_Suite.scala
@@ -0,0 +1,28 @@
+import chapter5.BitInsertion_5_1
+import org.scalatest.FunSuite
+
+class BitInsertion_5_1_Suite extends FunSuite {
+  test("Test Set 1") {
+    val N = Integer.parseInt("10000000000", 2)
+    val M = Integer.parseInt("10011", 2)
+    val actual = BitInsertion_5_1.insertBit(N, M, 2, 6)
+    val expected = Integer.parseInt("10001001100", 2)
+    assertResult(expected)(actual)
+  }
+
+  test("Test Set 2") {
+    val N = Integer.parseInt("10000000000", 2)
+    val M = Integer.parseInt("10011", 2)
+    val actual = BitInsertion_5_1.insertBit(N, M, 2, 3)
+    val expected = Integer.parseInt("10000001100", 2)
+    assertResult(expected)(actual)
+  }
+
+  test("Test Set 3") {
+    val N = Integer.parseInt("10001111100", 2)
+    val M = Integer.parseInt("10011", 2)
+    val actual = BitInsertion_5_1.insertBit(N, M, 2, 6)
+    val expected = Integer.parseInt("10001001100", 2)
+    assertResult(expected)(actual)
+  }
+}
