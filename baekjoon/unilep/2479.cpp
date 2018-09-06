f996325e25a2992628c213f9941ab02521361af9
sirius <unilep@naver.com>
2018-09-06 22:25:38 +0900

    Update 2479.cpp

--- baekjoon/unilep/2479.cpp
+++ baekjoon/unilep/2479.cpp
@@ -1,55 +1,64 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<queue>
+#include<stack>
 using namespace std;
 const int INF = 987654321;
-int N, K, A, B, D[1001][1001], P[1001];
-string s[1001];
+int N, K, A, B, P[1001], Q[1001];
+vector<int> V[1001];
 bool visit[1001];
-struct Node {
-    int no;
-    string ans;
-};
-int hamming_code(const string& a, const string& b) {
-    int cnt = 0;
-    for(int i=0; i<K; i++) {
-        if(a[i] != b[i]) cnt++;
-    }
-    return cnt;
+string s;
+
+bool hamming_code(int a, int b) {
+	int x = a ^ b;
+	return (x & (-x)) == x;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cin >> N >> K;
-    for(int i=1; i<=N; i++) {
-        cin >> s[i];
-        P[i] = i;
-    }
-    for(int i=1; i<=N; i++) {
-        for(int j=i+1; j<=N; j++) {
-            int cost = hamming_code(s[i], s[j]);
-            D[i][j] = D[j][i] = cost;
-        }
-    }
-    cin >> A >> B;
-    queue<Node> q;
-    q.push({A, to_string(A)});
-    visit[A] = true;
-    while(!q.empty()) {
-        Node node = q.front(); 
-        int no = node.no;
-        string ans = node.ans;
-        q.pop();
-        for(int i=1; i<=N; i++) {
-            if(no == i) continue;
-            if(visit[i]) continue;
-            if(D[i][no] != 1) continue;
-            q.push({ i, ans + " " + to_string(i)});
-            visit[i] = true;
-            if(i == B) {
-                cout << ans << " " + to_string(B);
-                return 0;
-            }
-        }
-    }
-    cout << -1;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++) {
+		cin >> s;
+		int x = 0;
+		for (int j = 0; j < K; j++) {
+			x *= 2;
+			x += (s[j] - '0');
+		}
+		Q[i] = x;
+		P[i] = i;
+	}
+	for (int i = 1; i <= N; i++){
+		for (int j = i + 1; j <= N; j++) {
+			if (hamming_code(Q[i], Q[j])) {
+				V[i].push_back(j);
+				V[j].push_back(i);
+			}
+		}
+	}
+	cin >> A >> B;
+	queue<int> q;
+	q.push(A);
+	visit[A] = true;
+	while (!q.empty()) {
+		int no = q.front(); q.pop();
+		for (int next : V[no]) {
+			if (visit[next]) continue;
+			q.push(next);
+			visit[next] = true;
+			P[next] = no;
+		}
+	}
+	if (!visit[B]) {
+		cout << -1;
+		return 0;
+	}
+	stack<int> st;
+	while (B != A) {
+		st.push(B);
+		B = P[B];
+	}
+	st.push(A);
+	while (!st.empty()) { cout << st.top() << " "; st.pop(); }
+
 }
