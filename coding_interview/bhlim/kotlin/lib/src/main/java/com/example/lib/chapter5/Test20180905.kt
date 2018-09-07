ca53e141a3e4f5d0d5c9b19c3c50d15cb89b399c
bhlim <byunghoolim@gmail.com>
2018-09-07 20:48:22 +0900

    5.5 solved

--- coding_interview/bhlim/kotlin/lib/src/main/java/com/example/lib/chapter5/Test20180905.kt
+++ coding_interview/bhlim/kotlin/lib/src/main/java/com/example/lib/chapter5/Test20180905.kt
@@ -8,22 +8,22 @@ fun Int.findSame1BitInteger() {
     var max = -1
     var min = -1
 
-    val srcCount = this.convertToBinary().getBit1Count()
+    val srcCount = this.convertToBinary(8).getBit1Count()
 
     print("Integer = $this , count  = $srcCount\n")
 
     for ( i in this + 1 until Int.MAX_VALUE) {
-        if(i.convertToBinary().getBit1Count() == srcCount) {
+        if(i.convertToBinary(8).getBit1Count() == srcCount) {
             min = i
-            print("found = $min , count  = ${i.convertToBinary().getBit1Count()}\n")
+            print("found = $min , count  = ${i.convertToBinary(8).getBit1Count()}\n")
             break
         }
     }
 
     for ( i in this - 1 downTo 0) {
-        if(i.convertToBinary().getBit1Count() == srcCount) {
+        if(i.convertToBinary(8).getBit1Count() == srcCount) {
             max = i
-            print("found = $max , count  = ${i.convertToBinary().getBit1Count()}\n")
+            print("found = $max , count  = ${i.convertToBinary(8).getBit1Count()}\n")
             break
         }
     }
