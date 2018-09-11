49742097c65d61c67c3d45a3f20504304462d1f5
Kimyongjin <yongjin5184@gmail.com>
2018-09-12 08:25:26 +0900

    백준 2485번 가로수 (틀렸지만 일단 커밋)

---
+++ coding_interview/kyj/src/Test2485.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * Created by yong on 2018. 9. 11..
+ */
+public class Test2485 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] streetTreesArr = new int[N];
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            streetTreesArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+//        int[] streetTreesArr = {1, 3, 7, 13};
+//        int[] streetTreesArr = {2, 6, 12, 18};
+
+        StreetTrees streetTrees = new StreetTrees(streetTreesArr);
+        int result = streetTrees.getSubtractArr().getMinValue().getOdd().getResult();
+//        streetTrees.printArr();
+//        System.out.println(streetTrees.num);
+//        System.out.println(streetTrees.num);
+        System.out.println(result);
+    }
+}
+
+class StreetTrees{
+
+    int[] streetTreesArr;
+    int[] subtractArr;
+
+    int minValue;
+    int num;
+
+    public StreetTrees(int[] streetTreesArr){
+        this.streetTreesArr = streetTreesArr;
+        subtractArr = new int[this.streetTreesArr.length - 1];
+
+    }
+
+    public StreetTrees getSubtractArr(){
+        for(int i = 0; i < streetTreesArr.length - 1; i++){
+            subtractArr[i] = streetTreesArr[i + 1] - streetTreesArr[i];
+        }
+
+        return this;
+    }
+
+    public StreetTrees getMinValue(){
+        Arrays.sort(subtractArr);
+        minValue = subtractArr[0];
+
+        return this;
+    }
+
+    public StreetTrees getOdd(){
+        num = minValue;
+        int length = subtractArr.length;
+        int i = 0;
+
+        while(i < length){
+            if(subtractArr[i] % num != 0){
+                num = num / 2;
+            }else{
+                i++;
+            }
+        }
+
+        return this;
+    }
+
+    public int getResult(){
+        int result = 0;
+        for(int i = 0; i < subtractArr.length; i++){
+            result += (subtractArr[i] / num) - 1;
+        }
+
+        return result;
+    }
+
+    public void printArr(){
+        for(int i = 0; i < subtractArr.length; i++){
+            if(subtractArr[i] != 0)
+                System.out.println(subtractArr[i]);
+        }
+    }
+}
