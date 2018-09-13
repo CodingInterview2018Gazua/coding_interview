2a8074ddfe4f49b6b3d07ed660252024286f13ef
amugaes <amugae.00001@gmail.com>
2018-09-13 18:52:25 +0900

    필요없는 주석 제거

--- baekjoon/lansun/15.bst_postorder_5639.py
+++ baekjoon/lansun/15.bst_postorder_5639.py
@@ -2,13 +2,6 @@
 # -*- coding: utf-8 -*-
 # python 2.7
 
-"""
-https://www.acmicpc.net/problem/5639
-"""
-
-# !/usr/bin/python
-# -*- coding: utf-8 -*-
-# python 2.7
 
 """
 https://www.acmicpc.net/problem/5639
