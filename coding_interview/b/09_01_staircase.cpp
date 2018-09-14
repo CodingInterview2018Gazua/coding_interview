5404fe8545a1112a381ffd46874e521bb68a2f4b
bruce <bruce.kim.it@gmail.com>
2018-09-14 23:00:25 +0900

    09 01 all steps

--- coding_interview/b/09_01_staircase.cpp
+++ coding_interview/b/09_01_staircase.cpp
@@ -2,10 +2,18 @@
 #include <iostream>
 using namespace std;
 /*
-1132436852
-memo time: 4.0801e-05
-1132436852
-brute time: 9.7364
+
+1 or 2 or 3 steps ok--
+53798080
+memo time: 1.2893e-05
+53798080
+brute time: 0.462665
+all steps ok--
+117897839
+memo time: 3.084e-06
+536870912
+brute time: 5.4808
+
 */
 
 const static int ARR_MAX = 1'000'000;
@@ -31,12 +39,40 @@ int howtosteps_memo(int n, int arr[]) {
     arr[n] = howtosteps_memo(n - 1, arr) + howtosteps_memo(n - 2, arr) + howtosteps_memo(n - 3, arr);
     return arr[n];
 }
+int howtostepsall(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    int sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        sum += howtostepsall(n - i);
+    }
+    return sum;
+}
+int howtostepsall_memo(int n, int arr[]) {
+    if (n == 0) {
+        arr[n] = 1;
+        return 1;
+    }
+    int sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (arr[i] != 0) {
+            sum += arr[i];
+
+        } else {
+            arr[i] = howtostepsall_memo(n - i, arr);
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
 int main() {
-    int n = 35;
+    int n = 30;
 
     std::chrono::high_resolution_clock::time_point t1, t2;
     std::chrono::duration<double> diff;
 
+    cout << "1 or 2 or 3 steps ok--" << endl;
     /* memoziation */
     int arr[ARR_MAX] = {0};
     t1 = std::chrono::high_resolution_clock::now();
@@ -52,5 +88,17 @@ int main() {
     diff = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
     cout << "brute time: " << diff.count() << endl;
 
+    cout << "all steps ok--" << endl;
+    t1 = std::chrono::high_resolution_clock::now();
+    cout << howtostepsall_memo(n, arr) << endl;
+    t2 = std::chrono::high_resolution_clock::now();
+    diff = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    cout << "memo time: " << diff.count() << endl;
+
+    t1 = std::chrono::high_resolution_clock::now();
+    cout << howtostepsall(n) << endl;
+    t2 = std::chrono::high_resolution_clock::now();
+    diff = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    cout << "brute time: " << diff.count() << endl;
     return 0;
 }
