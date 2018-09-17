aaee1037a2cd4b16deb5c14079c550f2d7ec06f1
amugaes <amugae.00001@gmail.com>
2018-09-17 23:48:46 +0900

    9.1 버그 수정(thanks to unilep)

--- coding_interview/lansun/chapter9/9.1_recursion_stairs.py
+++ coding_interview/lansun/chapter9/9.1_recursion_stairs.py
@@ -9,22 +9,24 @@ cache dict 에 recursion을 저장
 
 stairs = {1: 1, 2: 2}
 def upstair_count(n):
-    if n < 0: return 1
+    if n == 0: return 1
     if n not in stairs:
         stairs[n] = upstair_count(n - 1) + upstair_count(n - 2) + upstair_count(n - 3)
 
     return stairs[n]
 
 
-def test(n):
-    print "{} : {}".format(n, upstair_count(n))
-
-
-def main():
-    test(5)
-    test(10)
-    test(100)
-
-
-main()
+print upstair_count(3)
+# def test(n):
+#     print "{} : {}".format(n, upstair_count(n))
+#
+#
+# def main():
+#     # test(3)
+#     test(5)
+#     test(10)
+#     test(100)
+#
+#
+# main()
 
--- coding_interview/lansun/chapter9/9.2_robot_grid.py
+++ coding_interview/lansun/chapter9/9.2_robot_grid.py
@@ -4,10 +4,12 @@
 
 
 def robot_grid_path(x, y):
+    if not x and not y: return 1
     if x == 1 or y == 1: return 1
+    # if x < 0 or y < 0: return
 
     return robot_grid_path(x - 1, y) + robot_grid_path(x, y - 1)
 
 x = 3
-y = 3
+y = 4
 print '(0, 0) to ({}, {}) : {}'.format(x, y, robot_grid_path(x, y))
