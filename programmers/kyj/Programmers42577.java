42e9c0f95c127b9ff2ca92bc1903b659a76d3788
김용진 <yongjin@st-company.com>
2018-09-21 18:18:45 +0900

    프로그래머스 42577번 전화번호 목록, 일단 hash 없이 for문으로 풀이

--- programmers/kyj/Programmers42577.java
+++ programmers/kyj/Programmers42577.java
@@ -6,7 +6,7 @@ import java.util.Map;
  */
 public class Programmers42577 {
     public static void main(String[] args){
-        String[] phone_book = {"166", "16", "165"};
+        String[] phone_book = {"00", "10", "165"};
 //        String[] phone_book = {"123", "456", "789"};
 //        String[] phone_book = {"125", "1223", "1235", "567", "88"};
 
@@ -15,13 +15,12 @@ public class Programmers42577 {
         String str;
         boolean result = true;
         Loop1: for(int i = 0; i < phoneBookLength; i++){
-            str = phone_book[i];
+            str = phone_book[i].trim();
             for(int j = 0; j < phoneBookLength; j++){
                 if(i == j){
                     continue;
                 }
-
-                if(phone_book[j].contains(str)) {
+                if(str.startsWith(phone_book[j].trim())) {
                     result = false;
                     break Loop1;
                 }
