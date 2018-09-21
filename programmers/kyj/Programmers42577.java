a81f9aa2ab3ae013407ceab7d2d9c8bfe7fe5e7e
김용진 <yongjin@st-company.com>
2018-09-21 17:38:20 +0900

    우선 commit

---
+++ programmers/kyj/Programmers42577.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yongjin on 2018-09-21.
+ */
+public class Programmers42577 {
+    public static void main(String[] args){
+        String[] phone_book = {"119", "97674223", "1195524421"};
+
+        int phoneBookLength = phone_book.length;
+
+    }
+}
