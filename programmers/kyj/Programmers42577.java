2b8ef0d461688351fc284b3d4a98539ead63b5a9
김용진 <yongjin@st-company.com>
2018-09-21 18:01:50 +0900

    우선 commit

--- programmers/kyj/Programmers42577.java
+++ programmers/kyj/Programmers42577.java
@@ -1,11 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by yongjin on 2018-09-21.
  */
 public class Programmers42577 {
     public static void main(String[] args){
-        String[] phone_book = {"119", "97674223", "1195524421"};
+        String[] phone_book = {"1192219", "97674223", "119"};
+//        String[] phone_book = {"123", "456", "789"};
+//        String[] phone_book = {"125", "1223", "1235", "567", "88"};
+
+        Map<String, Integer> hm = new HashMap<>();
 
         int phoneBookLength = phone_book.length;
+        for(int i = 0; i < phoneBookLength; i++){
+            hm.put(phone_book[i], 0);
+        }
 
+        String str;
+        boolean result = true;
+        for(int i = 0; i < phoneBookLength; i++){
+            str = phone_book[i];
+            for(int j = 0; j < phoneBookLength; j++){
+                if(i == j){
+                    continue;
+                }
+
+                if(phone_book[j].contains(str)) {
+                    result = false;
+                }
+            }
+        }
+
+        System.out.println(result);
     }
 }
