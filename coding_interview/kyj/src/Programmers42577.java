380d5277b93648d29f3497af10c84242873928ef
김용진 <yongjin@st-company.com>
2018-09-21 18:08:41 +0900

    우선 commit

--- coding_interview/kyj/src/Programmers42577.java
+++ coding_interview/kyj/src/Programmers42577.java
@@ -6,20 +6,15 @@ import java.util.Map;
  */
 public class Programmers42577 {
     public static void main(String[] args){
-        String[] phone_book = {"1192219", "97674223", "119"};
+        String[] phone_book = {"166", "16", "165"};
 //        String[] phone_book = {"123", "456", "789"};
 //        String[] phone_book = {"125", "1223", "1235", "567", "88"};
 
-        Map<String, Integer> hm = new HashMap<>();
-
         int phoneBookLength = phone_book.length;
-        for(int i = 0; i < phoneBookLength; i++){
-            hm.put(phone_book[i], 0);
-        }
 
         String str;
         boolean result = true;
-        for(int i = 0; i < phoneBookLength; i++){
+        Loop1: for(int i = 0; i < phoneBookLength; i++){
             str = phone_book[i];
             for(int j = 0; j < phoneBookLength; j++){
                 if(i == j){
@@ -28,6 +23,7 @@ public class Programmers42577 {
 
                 if(phone_book[j].contains(str)) {
                     result = false;
+                    break Loop1;
                 }
             }
         }
