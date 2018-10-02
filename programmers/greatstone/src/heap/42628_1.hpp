b89782c470bfc9ccf25c742de09e4e8c9d11bf88
greatstone <doribugi@users.noreply.github.com>
2018-10-03 01:56:49 +0900

    Create 42628_1.hpp

---
+++ programmers/greatstone/src/heap/42628_1.hpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(vector<string> operations) {
+    multiset<int> ms;
+    for (string operation : operations) {
+        char command;
+        int number;
+        sscanf(operation.c_str(), "%c %d", &command, &number);
+        if (command == 'I') {
+            ms.insert(number);
+        } else if (command == 'D' && !ms.empty()) {
+            if (number == 1) {
+                ms.erase(--ms.end());
+            } else if (number == -1) {
+                ms.erase(ms.begin());
+            }
+        }
+    }
+    vector<int> answer;
+    if (ms.empty()) {
+        answer.push_back(0);
+        answer.push_back(0);
+    } else {
+        answer.push_back(*(--ms.end()));
+        answer.push_back(*ms.begin());
+    }
+    return answer;
+}
