f2d3037cdb5130e4ecd41ffd0cc42528fe5ca76b
sirius <unilep@naver.com>
2018-10-03 16:48:06 +0900

    Update bar.cpp

--- programmers/unilep/stack_queue/bar.cpp
+++ programmers/unilep/stack_queue/bar.cpp
@@ -3,6 +3,13 @@
 #include<iostream>
 using namespace std;
 
+/*
+    () 가 나올 경우 레이저를 쏘기 때문에 현재 스택사이즈 만큼 정답이 증가한다.
+    ( 가 나올 경우 스택 사이즈를 증가시키고
+    ) 가 나올 경우 스택 사이즈를 감소시키고 정답이 1 증가한다.
+    왜냐하면 -|-  이것은 2 이기 때문
+*/
+
 int solution(string arrangement) {
     int answer = 0;
     int stack = 0;
