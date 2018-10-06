82062e740c62fb4a867e6bcafe377dd92b54864f
Kimyongjin <yongjin5184@gmail.com>
2018-10-07 00:01:25 +0900

    프로그래머스 42587번 스택/큐 기능개발

--- programmers/kyj/Programmers42586.java
+++ programmers/kyj/Programmers42586.java
@@ -10,7 +10,14 @@ public class Programmers42586 {
     public static void main(String[] args) throws IOException {
         /**
          * @description : 프로그래머스 42587번 스택/큐 기능개발
-         * @solution :
+         * @solution : 1. 우선순위를 가지고 있는 큐와 개발 속도를 가지고 있는 큐를 만든다.
+         *             2. 우선순위를 가지고 있는 큐의 맨 앞부터 개발 완성도가 100%에 달할때 까지 Percentage를 더해준다.
+         *             3. 맵을 만들고 소요일자를 KEY로 만들어 배포되는 기능의 갯수를 같이 세어 Return 한다.\
+         *             * 처음에 Hashmap을 만들어서 버그가 있었는데 HashMap은 순서대로 데이터를 가져오는 걸 보장하지 못한다.
+         *             따라서 순서대로 데이터를 가져오고 싶으 LinkedHashMap과 TreeMap을 사용하면된다.
+         *             LinkedHashMap: 입력된 순서대로 데이터가 출력
+         *             TreeMap: 입력된 key의 소트순으로 데이터가 출력
+         *
          */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -29,7 +36,7 @@ public class Programmers42586 {
     public static int[] solution(int[] progresses, int[] speeds) {
         Queue<Integer> progressQueue = new LinkedList<>();
         Queue<Integer> speedQueue = new LinkedList<>();
-        HashMap<Integer, Integer> hm = new HashMap();
+        Map<Integer, Integer> hm = new TreeMap();
 
         for(int i = 0; i < progresses.length; i++){
             progressQueue.offer(progresses[i]);
