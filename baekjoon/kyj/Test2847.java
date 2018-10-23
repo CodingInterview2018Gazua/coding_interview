0d3adfea7c9c6b100d195694e75ec1cbd4c6cbe6
Kimyongjin <yongjin5184@gmail.com>
2018-10-23 23:17:44 +0900

    코드 간략화

--- baekjoon/kyj/Test2847.java
+++ baekjoon/kyj/Test2847.java
@@ -23,7 +23,7 @@ public class Test2847 {
         for(int i = N - 1; i > 0; i--){
             if(arr[i] <= arr[i - 1]){
                 num += - ((arr[i] - arr[i - 1]) - 1);
-                arr[i - 1] += ((arr[i] - arr[i - 1]) - 1);
+                arr[i - 1] = arr[i] - 1;
             }
         }
 
