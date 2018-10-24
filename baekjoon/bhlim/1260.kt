a3163902987ac49aa2f1d74c83f8a963bb676a0f
bhlim <byunghoolim@gmail.com>
2018-10-24 19:16:09 +0900

    ,

--- baekjoon/bhlim/1260.kt
+++ baekjoon/bhlim/1260.kt
@@ -11,7 +11,8 @@ import kotlin.collections.HashMap
 //    var arr = ArrayList<Int>()
 //
 //    arr.add(2)
-//    arr.add(5)
+//    arr.add(3)
+//    arr.add(4)
 //
 //    datas[1] = arr
 //
@@ -20,9 +21,7 @@ import kotlin.collections.HashMap
 //    arr = ArrayList()
 //
 //    arr.add(1)
-//    arr.add(3)
 //    arr.add(4)
-//    arr.add(5)
 //
 //    datas[2] = arr
 //
@@ -30,7 +29,7 @@ import kotlin.collections.HashMap
 //
 //    arr = ArrayList()
 //
-//    arr.add(2)
+//    arr.add(1)
 //    arr.add(4)
 //
 //    datas[3] = arr
@@ -39,42 +38,57 @@ import kotlin.collections.HashMap
 //
 //    arr = ArrayList()
 //
-//    arr.add(3)
-//    arr.add(5)
-//    arr.add(2)
-//    arr.add(6)
-//
-//    datas[4] = arr
-//
-//    ////////////
-//
-//    arr = ArrayList()
-//
 //    arr.add(1)
 //    arr.add(2)
-//    arr.add(4)
 //
-//    datas[5] = arr
-//
-//    ////////////
-//
-//    arr = ArrayList()
-//
-//    arr.add(4)
-//
-//    datas[6] = arr
+//    datas[4] = arr
 //
 //    datas.dfs(1)
 //    datas.bfs(1)
 //}
 
 fun main(args: Array<String>) {
-    val sc = Scanner(System.`in`)
-    val array = ArrayList<String>()
 
-    while (sc.hasNextLine()) {
-        array.add(sc.nextLine())
-    }
+    val array = ArrayList<String>()
+    //val sc = Scanner(System.`in`)
+
+
+//    while (sc.hasNextLine()) {
+//        array.add(sc.nextLine())
+//    }
+
+//    array.add("4 5 1")
+//    array.add("1 2")
+//    array.add("1 3")
+//    array.add("1 4")
+//    array.add("2 4")
+//    array.add("3 4")
+
+    array.add("4 5 1")
+
+    array.add("1 2")
+    array.add("1 5")
+
+    array.add("2 1")
+    array.add("2 3")
+    array.add("2 4")
+    array.add("2 5")
+
+    array.add("3 2")
+    array.add("3 4")
+
+    array.add("4 3")
+    array.add("4 5")
+    array.add("4 2")
+    array.add("4 6")
+
+    array.add("5 1")
+    array.add("5 2")
+    array.add("5 4")
+
+    array.add("6 4")
+
+
 
     val startLine = array[0]
 
@@ -90,25 +104,33 @@ fun main(args: Array<String>) {
         val start = splited[0].toInt()
         val dest = splited[1].toInt()
 
-        val arr : ArrayList<Int>?
-
-        if(datas.containsKey(start)) {
-            arr = datas[start]
-
-            if(arr?.contains(dest) == false) {
-                arr.add(dest)
-                datas[start] = arr
-            }
-        } else {
-            arr = ArrayList()
-            arr.add(dest)
-
-            datas[start] = arr
-        }
+        datas.addNode(start, dest)
     }
 
     datas.dfs(startIdx)
     datas.bfs(startIdx)
+}
+
+private fun HashMap<Int, ArrayList<Int>>.addNode(start : Int, dest : Int) {
+    val arr : ArrayList<Int>?
+
+    if(containsKey(start)) {
+        arr = this[start]
+
+        if(arr?.contains(dest) == false) {
+            arr.add(dest)
+            this[start] = arr
+
+            this.addNode(dest, start)
+        }
+    } else {
+        arr = ArrayList()
+        arr.add(dest)
+
+        this[start] = arr
+
+        this.addNode(dest, start)
+    }
 }
 
 private fun Map<Int, ArrayList<Int>>.dfs(start : Int) {
