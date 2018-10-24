a7de900995043cefda017f9d77a6c2f2a0e90819
bhlim <byunghoolim@gmail.com>
2018-10-24 18:25:02 +0900

    ,

--- baekjoon/bhlim/1260.kt
+++ baekjoon/bhlim/1260.kt
@@ -1,84 +1,115 @@
 package com.example.hoo.baekjoon
 
 import java.util.*
+import java.util.concurrent.LinkedBlockingQueue
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
-fun main(args: Array<String>) {
-    val datas = HashMap<Int, ArrayList<Int>>()
-
-    var arr = ArrayList<Int>()
-
-    arr.add(2)
-    arr.add(3)
-    arr.add(4)
-    arr.add(2)
-
-    datas[1] = arr
-
-    ///////////
-
-    arr = ArrayList()
-
-    arr.add(4)
-    arr.add(4)
-
-    datas[2] = arr
-
-    ////////////
-
-    arr = ArrayList()
-
-    arr.add(4)
-
-    datas[3] = arr
-
-    datas.dfs(1)
-    datas.bfs(1)
-}
-
 //fun main(args: Array<String>) {
-//    val sc = Scanner(System.`in`)
-//    val array = ArrayList<String>()
-//
-//    while (sc.hasNextLine()) {
-//        array.add(sc.nextLine())
-//    }
-//
-//    val startLine = array[0]
-//
-//    array.removeAt(0)
-//
-//    val startIdx = (startLine.split(" "))[2].toInt()
-//
 //    val datas = HashMap<Int, ArrayList<Int>>()
 //
-//    array.forEach {
-//        val splited = it.split(" ")
+//    var arr = ArrayList<Int>()
 //
-//        val start = splited[0].toInt()
-//        val dest = splited[1].toInt()
+//    arr.add(2)
+//    arr.add(5)
 //
-//        val arr : ArrayList<Int>?
+//    datas[1] = arr
 //
-//        if(datas.containsKey(start)) {
-//            arr = datas[start]
+//    ///////////
 //
-//            if(arr?.contains(dest) == false) {
-//                arr.add(dest)
-//                datas[start] = arr
-//            }
-//        } else {
-//            arr = ArrayList()
-//            arr.add(dest)
+//    arr = ArrayList()
 //
-//            datas[start] = arr
-//        }
-//    }
+//    arr.add(1)
+//    arr.add(3)
+//    arr.add(4)
+//    arr.add(5)
 //
-//    datas.dfs(startIdx)
-//    datas.bfs(startIdx)
+//    datas[2] = arr
+//
+//    ////////////
+//
+//    arr = ArrayList()
+//
+//    arr.add(2)
+//    arr.add(4)
+//
+//    datas[3] = arr
+//
+//    ////////////
+//
+//    arr = ArrayList()
+//
+//    arr.add(3)
+//    arr.add(5)
+//    arr.add(2)
+//    arr.add(6)
+//
+//    datas[4] = arr
+//
+//    ////////////
+//
+//    arr = ArrayList()
+//
+//    arr.add(1)
+//    arr.add(2)
+//    arr.add(4)
+//
+//    datas[5] = arr
+//
+//    ////////////
+//
+//    arr = ArrayList()
+//
+//    arr.add(4)
+//
+//    datas[6] = arr
+//
+//    datas.dfs(1)
+//    datas.bfs(1)
 //}
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val array = ArrayList<String>()
+
+    while (sc.hasNextLine()) {
+        array.add(sc.nextLine())
+    }
+
+    val startLine = array[0]
+
+    array.removeAt(0)
+
+    val startIdx = (startLine.split(" "))[2].toInt()
+
+    val datas = HashMap<Int, ArrayList<Int>>()
+
+    array.forEach {
+        val splited = it.split(" ")
+
+        val start = splited[0].toInt()
+        val dest = splited[1].toInt()
+
+        val arr : ArrayList<Int>?
+
+        if(datas.containsKey(start)) {
+            arr = datas[start]
+
+            if(arr?.contains(dest) == false) {
+                arr.add(dest)
+                datas[start] = arr
+            }
+        } else {
+            arr = ArrayList()
+            arr.add(dest)
+
+            datas[start] = arr
+        }
+    }
+
+    datas.dfs(startIdx)
+    datas.bfs(startIdx)
+}
 
 private fun Map<Int, ArrayList<Int>>.dfs(start : Int) {
     val resultArr = ArrayList<Int>()
@@ -101,7 +132,7 @@ private fun Map<Int, ArrayList<Int>>.dfs(start : Int) {
                 val dests = this[pop]
 
                 dests?.forEach {
-                    if(!resultArr.contains(it))
+                    if(!resultArr.contains(it) && !stack.contains(it))
                         stack.push(it)
                 }
             }
@@ -114,7 +145,7 @@ private fun Map<Int, ArrayList<Int>>.dfs(start : Int) {
 
 private fun Map<Int, ArrayList<Int>>.bfs(start : Int) {
     val resultArr = ArrayList<Int>()
-    val queue = PriorityQueue<Int>()
+    val queue = LinkedBlockingQueue<Int>()
 
     resultArr.add(start)
 
@@ -135,7 +166,7 @@ private fun Map<Int, ArrayList<Int>>.bfs(start : Int) {
             val dests = this[pop]
 
             dests?.forEach {
-                if(!resultArr.contains(it))
+                if(!resultArr.contains(it) && !queue.contains(it))
                     queue.add(it)
             }
         }
