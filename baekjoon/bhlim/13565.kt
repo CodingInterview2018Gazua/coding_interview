cf6091557dfc218f6b8f4c676a8b5ac169de051b
bhlim <byunghoolim@gmail.com>
2018-10-29 12:34:12 +0900

    13565 complete

--- baekjoon/bhlim/13565.kt
+++ baekjoon/bhlim/13565.kt
@@ -6,27 +6,39 @@ import kotlin.collections.ArrayList
 val visited = ArrayList<Pair<Int,Int>>()
 
 fun main(args: Array<String>) {
+    val source = ArrayList<String>()
+    val sc = Scanner(System.`in`)
 
-    val input = Array(5) { Array(6) { 0 } }
+    while (sc.hasNextLine()) {
+        source.add(sc.nextLine())
+    }
 
-    input[0][1] = 1
-    input[0][3] = 1
-    input[0][5] = 1
+//    source.add("8 8")
+//    source.add("11000111")
+//    source.add("01100000")
+//    source.add("00011001")
+//    source.add("11001000")
+//    source.add("10001001")
+//    source.add("10111100")
+//    source.add("01010000")
+//    source.add("00001011")
 
-    input[1][1] = 1
+    val inputSize = source[0].split(" ")
 
-    input[2][1] = 1
-    input[2][2] = 1
-    input[2][3] = 1
-    input[2][5] = 1
+    val input = Array(inputSize[0].toInt()) { Array(inputSize[1].toInt()) { 0 } }
 
-    input[3][0] = 1
-    input[3][4] = 1
-    input[3][5] = 1
+    source.removeAt(0)
 
-    input[4][2] = 1
-    input[4][4] = 1
-    input[4][5] = 1
+    for(i in 0 until source.size) {
+        val arr = source[i].toCharArray()
+        val row = input[i]
+
+        arr.forEachIndexed { index, s ->
+            row[index] = arr[index].toString().toInt()
+        }
+
+        input[i] = row
+    }
 
     input.findOut()
 }
@@ -34,65 +46,89 @@ fun main(args: Array<String>) {
 private fun Array<Array<Int>>.findOut() {
     val stack = Stack<Pair<Int,Int>>()
 
-    for(i in 0 until this[0].size) {
-        val visit = Pair(0, i)
+    if(this.size == 1) {
+        this[0].forEach {
+            if(it == 0) {
+                print("YES\n")
+                return
+            }
+        }
+    } else {
+        for(i in 0 until this[0].size) {
+            val visit = Pair(0, i)
 
-        if(!visited.contains(visit) && this[visit.first][visit.second] == 1 ) {
-            stack.push(visit)
+            if(!visited.contains(visit) && this[visit.first][visit.second] == 0 ) {
+                stack.push(visit)
 
-            while (stack.isNotEmpty()) {
-                val pop = stack.pop()
-                visited.add(pop)
+                while (stack.isNotEmpty()) {
+                    val pop = stack.pop()
+                    visited.add(pop)
 
-                val list = this.canGoList(pop)
+                    this.canGoList(pop).forEach {
+                        if(it.first == this.size -1 && this[it.first][it.second] == 0) {
+                            print("YES\n")
+                            return
+                        }
 
-                list.forEach {
-                    if(it.first == this.size -1 && this[it.first][it.second] == 1) {
-                        print("YES")
-                        return
-                    }
-
-                    if(!visited.contains(it)) {
-                        stack.push(it)
+                        if(!visited.contains(it)) {
+                            stack.push(it)
+                        }
                     }
                 }
             }
         }
     }
 
-    print("NO")
+    print("NO\n")
 }
 
 private fun Array<Array<Int>>.canGoList(point : Pair<Int,Int>) : ArrayList<Pair<Int,Int>>{
     val result = ArrayList<Pair<Int,Int>>()
     val (first, sec) = point
 
-    if(first != 0) {
-        val up = this[first -1][sec]
-
-        if(up == 1)
-            result.add(Pair(first -1, sec))
-    }
-
-    if(first != this.size -1) {
-        val down = this[first + 1][sec]
-
-        if(down == 1)
-            result.add(Pair(first +1, sec))
-    }
-
     if(sec != 0) {
         val left = this[first ][sec - 1]
 
-        if(left == 1)
-            result.add(Pair(first, sec -1))
+        if(left == 0) {
+            val leftP = Pair(first, sec -1)
+
+            if(!visited.contains(leftP))
+                result.add(leftP)
+        }
+    }
+
+    if(first != 0) {
+        val up = this[first -1][sec]
+
+        if(up == 0) {
+            val upP = Pair(first -1, sec)
+
+            if(!visited.contains(upP))
+                result.add(upP)
+        }
+
     }
 
     if(sec != this[0].size -1) {
         val right = this[first][sec + 1]
 
-        if(right == 1)
-            result.add(Pair(first, sec + 1))
+        if(right == 0) {
+            val rightP = Pair(first, sec + 1)
+
+            if(!visited.contains(rightP))
+                result.add(rightP)
+        }
+    }
+
+    if(first != this.size -1) {
+        val down = this[first + 1][sec]
+
+        if(down == 0) {
+            val downP = Pair(first +1, sec)
+
+            if(!visited.contains(downP))
+                result.add(downP)
+        }
     }
 
     return result
