aaa9980a58eb7ef19769dc238a0ee141633ee190
bhlim <byunghoolim@gmail.com>
2018-11-12 20:20:09 +0900

    15786 solved

--- baekjoon/bhlim/1026.kt
+++ baekjoon/bhlim/1026.kt
@@ -4,18 +4,24 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val source = ArrayList<String>()
-    val sc = Scanner(System.`in`)
+//    val sc = Scanner(System.`in`)
+//
+//    while (sc.hasNextLine()) {
+//        source.add(sc.nextLine())
+//    }
+//
+//    source.removeAt(0)
 
-    while (sc.hasNextLine()) {
-        source.add(sc.nextLine())
-    }
 
-    source.removeAt(0)
+    source.add("1 1 1 6 0")
+    source.add("2 7 8 3 1")
 
     var sum = 0
 
     val A = source[0].split(" ").map { it.toInt() }.toIntArray()
     val B = source[1].split(" ").map { it.toInt() }.toIntArray()
+
+    print("${Arrays.toString(A)}\n${Arrays.toString(B)}")
 
     A.sortDescending()
     B.sort()
