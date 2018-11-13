859eb5ee8485adab5d6ba29695ab7e9a706b5b9f
amugaes <amugae.00001@gmail.com>
2018-11-13 20:38:23 +0900

    6131 변수명 변경

--- baekjoon/lansun/19.perfect_square_number_6131.py
+++ baekjoon/lansun/19.perfect_square_number_6131.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 # python 2.7
 
+# A의 제곱은 B의 제곱보다 N만큼 커 (1 ≤ N ≤ 1,000)
+# 위의 힌트 조건을 만족하는 A와 B 쌍의 개수를 구하는 프로그램을 작성하시오.
+# 상근이의 힌트 조건을 만족하는 (A,B) 쌍의 개수를 출력한다.
+
 result = 0
 end = 501
 n = int(raw_input())
-for i in range(1, end):
-    for j in range(i, end):
-        result += j ** 2 - i ** 2 == n
+for b in range(1, end):
+    for a in range(b, end):
+        result += a ** 2 - b ** 2 == n
 print result
