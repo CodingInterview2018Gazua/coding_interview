1277405bf3b46e8402231d4a8e651d98869ee70e
bhlim <byunghoolim@gmail.com>
2018-11-23 17:06:31 +0900

    9184

--- baekjoon/bhlim/9184.kt
+++ baekjoon/bhlim/9184.kt
@@ -2,46 +2,32 @@ package com.example.hoo.rxjava.baekjoon
 
 import java.util.*
 
-private val dp = Array(21) { Array(21) { IntArray(21) { 0 } } }
+private val dp = Array(21) { Array(21) { IntArray(21) { -1 } } }
 
 fun main(args: Array<String>) {
-    val source = ArrayList<String>()
     val sc = Scanner(System.`in`)
 
-    while (sc.hasNextLine()) {
-        source.add(sc.nextLine())
-    }
+    while (true) {
+        val a = sc.nextInt()
+        val b = sc.nextInt()
+        val c = sc.nextInt()
 
-//    source.add("1 1 1")
-//    source.add("2 2 2")
-//    source.add("21 21 21")
-//    source.add("50 50 50")
-//    source.add("-1 7 18")
-//    source.add("-1 -1 -1")
+        if (a == -1 && b == -1 && c == -1)
+            break
 
-    source.filter {
-        it != "-1 -1 -1"
-    }.map {
-        it.split(" ").map { innerIt ->
-            innerIt.toInt()
-        }
-    }.forEach {
-        val a = it[0]
-        val b = it[1]
-        val c = it[2]
-
-        print("w($a, $b, $c) = ${w(a, b, c)}\n")
+        println("w($a, $b, $c) = ${w(a, b, c)}")
     }
 }
 
 private fun w(a: Int, b: Int, c: Int): Int {
     return when {
         a <= 0 || b <= 0 || c <= 0 -> 1
-        a > 20 || b > 20 || c > 20 -> {
-            dp[20][20][20] = w(20, 20, 20)
-            dp[20][20][20]
+        a > 20 || b > 20 || c > 20 -> w(20, 20, 20)
+        dp[a][b][c] != -1 -> dp[a][b][c]
+        a < b && b < c -> {
+            dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+            dp[a][b][c]
         }
-        dp[a][b][c] != 0 -> dp[a][b][c]
         else -> {
             dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
             dp[a][b][c]
