1294f92250c887f4298c50503f4e37f698c2f297
Kimyongjin <yongjin5184@gmail.com>
2018-12-06 21:39:21 +0900

    백준 14696번 딱지놀이

--- baekjoon/kyj/src/Test14696.java
+++ baekjoon/kyj/src/Test14696.java
@@ -1,52 +1,45 @@
-package src;
-
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-
+/**
+ * Created by yong on 2018. 12. 3..
+ */
 public class Test14696 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         /**
-         *
+         * description : 백준 14696번 딱지놀이
+         * solution : 1. 4까지 표현할 수 있는 들어가는 배열 선언 뒤, for문을 돌리면서 인덱스에 값을 늘려서 비교.
          */
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("test.txt")));
-        int N = Integer.parseInt(br.readLine());
-
-        for(int i = 0; i < N; i++) {
-
-            int[] tmp1 = new int[5];
-            int[] tmp2 = new int[5];
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int gameCnt = Integer.parseInt(br.readLine());
+        for(int i = 0; i < gameCnt; i++){
             String[] strs1 = br.readLine().trim().split(" ");
-            int[] arr1 = Arrays.stream(strs1).mapToInt(Integer::parseInt).toArray();
+            int[] arr = Arrays.stream(strs1).mapToInt(Integer::parseInt).toArray();
+            int[] myArr = new int[5];
+            for(int j = 1; j < arr.length; j++){
+                myArr[arr[j]]++;
+            }
 
             String[] strs2 = br.readLine().trim().split(" ");
             int[] arr2 = Arrays.stream(strs2).mapToInt(Integer::parseInt).toArray();
-
-            for(int j = 1; j <= arr1[0]; j++){
-                tmp1[arr1[j]]++;
-            }
-
-            for(int j = 1; j <= arr2[0]; j++){
-                tmp2[arr2[j]]++;
+            int[] friendArr = new int[5];
+            for(int j = 1; j < arr2.length; j++){
+                friendArr[arr2[j]]++;
             }
 
             String result = "D";
-            for(int j = tmp2.length - 1; j > 0; j--){
-                if(tmp1[j] > tmp2[j]) {
+            for(int j = myArr.length - 1; j >= 1; j--){
+                if(myArr[j] > friendArr[j]) {
                     result = "A";
                     break;
-                }else if(tmp1[j] == tmp2[j]){
-                    continue;
-                }else{
+                } else if(myArr[j] < friendArr[j]) {
                     result = "B";
                     break;
                 }
             }
 
-            System.out.print(result);
+            System.out.println(result);
         }
     }
 }
