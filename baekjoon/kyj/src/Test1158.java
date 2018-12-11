47889c2de9f786b3dc5e1fdd0730dc9ef5da3584
Kimyongjin <yongjin5184@gmail.com>
2018-12-11 10:21:32 +0900

    백준 1158번 조세퍼스

--- baekjoon/kyj/src/Test1158.java
+++ baekjoon/kyj/src/Test1158.java
@@ -7,6 +7,13 @@ import java.util.StringTokenizer;
 
 public class Test1158 {
     public static void main(String[] args) throws IOException {
+        /**
+         * description : 백준 1158번 조세퍼스
+         * solution : 1. 양쪽 끝에서 삽입과 삭제가 가능한 Deque를 사용한다.
+         *            2. M - 1 만큼 Deque의 앞에서 값을 꺼내 뒤에다 삽입 한 뒤,
+         *            3. 맨 앞의 값을 StringBuilder에 append 시킨다.
+         *            4. 큐가 Empty 될 때까지 계속한다.
+         */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Deque<Integer> deque = new ArrayDeque<Integer>();
 
@@ -16,7 +23,7 @@ public class Test1158 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        for(int i = 1;i <= N; i++) {
+        for(int i = 1; i <= N; i++) {
             deque.add(i);
         }
 
