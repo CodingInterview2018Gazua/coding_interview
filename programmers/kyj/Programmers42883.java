3e062c6fe8fa4f85a744bdfdc624c85369f6d56a
Kimyongjin <yongjin5184@gmail.com>
2019-01-08 19:10:59 +0900

    프로그래머스 가장 큰 수 > 42883번

--- programmers/kyj/Programmers42883.java
+++ programmers/kyj/Programmers42883.java
@@ -2,50 +2,52 @@ import java.util.Arrays;
 
 public class Programmers42883 {
     public static void main(String[] args){
+        /**
+         * description : 프로그래머스 42883번 가장 큰 수
+         * solution : 1. 범위를 지정해주고 범위 안에서 가장 큰 수를 추출 하여 리턴하는 문자열에 더해준다.
+         *            2. 범위의 시작은 가장 큰 수가 나오면 시작값 (start 값을 바꿔주면서 진행)
+         */
 //        String number   = "1924";
 //        int    k        = 2;
 
-        String number   = "4177252841";
-        int    k        = 9;
+//        String number   = "4177252841";
+//        int    k        = 4;
 
 //        String number = "1231234";
 //        int    k        = 3;
 
-//        String number = "1000001";
-//        int    k        = 6;
+//         String number = "0";
+//         int    k        = 0;
+
+        String number = "123191001"; // 5
+        int    k        = 3;
 
         String answer = solution(number, k);
-        System.out.println(answer);
+        System.out.println(answer); //  31103
     }
 
     public static String solution(String number, int k) {
-        String answer = "";
+        int length = number.length() - k;
         int[] answerArr = Arrays.stream(number.split("")).mapToInt(Integer::parseInt).toArray();
-        int start = 0;
-        int length = number.length() - k; //최종 return value의 length
-        for(int i = 0; i < length; i++){
-            int max = 0;
-            int index = 0;
-            int leftLength =  length - answer.length();
-            for(int j = start; j < answerArr.length; j++) {
-                if(leftLength > answerArr.length - j){
-                    break;
-                }
+        StringBuilder answer = new StringBuilder();
 
-                if (max < answerArr[j]){
+        int start = 0;
+        int end = k + 1;
+
+        for (int i = 0; i < length; i++) {
+            int max = 0;
+            for (int j = start; j < end; j++) {
+                if (max < answerArr[j]) {
                     max = answerArr[j];
-                    index = j;
+                    start = j;
                 }
             }
 
-            start = index + 1;
-            answer += max;
+            start++;
+            end++;
+            answer.append(max);
         }
 
-        if(answer.equals("")){
-            answer += 0;
-        }
-
-        return answer;
+        return answer.toString();
     }
 }
