f5468d40c8d273a89be047ece9d149b1bfbc5795
Kimyongjin <yongjin5184@gmail.com>
2019-01-22 18:06:49 +0900

    프로그래머스 42895번 N으로 표현
    우선 Commit

---
+++ programmers/kyj/Programmers42895.java
@@ -0,0 +1,37 @@
+public class Programmers42895 {
+
+    public static int answer = -1;
+    public static void main(String[] args){
+        int cnt = solution(5, 12);
+        System.out.println(cnt);
+    }
+
+    public static int solution(int N, int number) {
+        dfs(N, 0, 0, number, "");
+        return answer;
+    }
+
+    public static void dfs(int n, int pos, int num, int number, String s) {
+        if (pos > 8)
+            return;
+
+        if (num == number) {
+            if (pos < answer || answer == -1) {
+                System.out.println(s);
+                answer = pos;
+            }
+            return;
+        }
+
+        int nn = 0;
+        for (int i = 0; i < 8; i++) {
+            nn = nn * 10 + n;
+            dfs(n, pos + 1 + i, num + nn, number, s + "+");
+            dfs(n, pos + 1 + i, num - nn, number, s + "-");
+            dfs(n, pos + 1 + i, num * nn, number, s + "*");
+            dfs(n, pos + 1 + i, num / nn, number, s + "/");
+        }
+        // dfs(n,pos+1,num*10+n,number,s+"5");
+    }
+
+}
